e125908777d0dc141d2bb58647fff8f134abc4ff
Alex Liebscher <alexliebscher0@gmail.com>
2019-11-11 15:17:12 -0800

    continue to build structure of modeling pieces

--- edge-probing-nonliteral/model.py
+++ edge-probing-nonliteral/model.py
@@ -1,8 +1,59 @@
 import torch as tt
+import torch.nn as nn
 import transformers as tm
 import numpy as np
 
+class Pooler(nn.Module):
+    """
+    Perform pooling, potentially with a projection layer beforehand.
+    """
 
-class TaskModel:
+    def __init__(self, project=True, pooling_type="mean"):
+        self._project = project
+        self._pooling_type = pooling_type
 
-    pass
+    def forward(self):
+        if self._pooling_type == "mean":
+            pass
+        elif self._pooling_type == "max":
+            pass
+
+        return None
+
+
+class Classifier(nn.Module):
+    """
+    Create a classifier for model output
+    """
+
+    def __init__(self, inp_dim, n_classes):
+        self._classifier = None
+
+    def forward(self):
+        logits = self._classifier()
+
+        return logits
+
+
+class EdgeProbingModel(nn.Module):
+    """
+    Given a task and dataset, train a model of the contextual embeddings encoded information.
+    """
+
+    def __init__(self):
+        self._span_extractor = None
+        self._classifier = None
+
+
+    def forward(self):
+        """
+        Forward pass for Edge Probing models. Extract the appropriate span(s) and classify them, compute loss.
+        """
+        out = dict()
+
+        span_emb = None
+        logits = self._classifier(span_emb)
+
+        out["logits"] = logits
+
+        return out
--- edge-probing-nonliteral/tests/test_model.py
+++ edge-probing-nonliteral/tests/test_model.py
@@ -1,5 +1,50 @@
 import pytest as pt
 
-class ModelTests:
+# import edge probing model.py
 
-    pass
+
+class TestPooler:
+    """
+    Test the Pooler class
+    """
+    def test_pooling_io_dims(self):
+        """
+        Test that the proper dimensions are going into and coming out of Pooler.
+        """
+        assert 1
+
+    def test_pooling_type(self):
+        """
+        Ensure that each pooling type is doing what we'd expect it to do.
+        """
+        assert 1
+
+
+class TestClassifier:
+    """
+    Test the Classifier class
+    """
+    def test_classifier_io_dims(self):
+        """
+        Test that the proper dimensions are going into and coming out of Classifier.
+        """
+        assert 1
+
+class TestModel:
+    """
+    Test the Model building and execution
+    """
+
+    def test_training_loss_movement(self):
+        """
+        Test that the model loss changes with a single step during training
+        """
+
+        assert 1
+
+    def test_loss_at_zero(self):
+        """
+        Make sure the loss is never exactly zero
+        """
+
+        assert 1
