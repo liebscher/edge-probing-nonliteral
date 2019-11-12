f8d60a131fe6debedf1cc968ba41342750b70744
Alex Liebscher <alexliebscher0@gmail.com>
2019-11-11 17:27:01 -0800

    transform into package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="edge-probing-nonliteral",
+    version="0.0.1",
+    author="Alex Liebscher",
+    author_email="aliebsch@ucsd.edu",
+    description="Edge Probing on Contextual Embeddings on Nonliteral Semantics Tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liebscher/edge-probing-nonliteral/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
