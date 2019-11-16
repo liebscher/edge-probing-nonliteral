8ec0b9477ec8f8ddf9566f8fd55fea02bcc12019
Alex Liebscher <alexliebscher0@gmail.com>
2019-11-15 17:18:46 -0800

    add logging, setup training, saving, and actual torch models

--- epnl/model.py
+++ epnl/model.py
@@ -1,24 +1,80 @@
+import logging, logging.config
+
+# create logger
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('epnl')
+
+import numpy as np
 import torch as tt
 import torch.nn as nn
+import torch.optim as op
 import transformers as tm
-import numpy as np
+
+import torch as tt
+import torch.nn as nn
+
+def get_criterion_loss_function(model, task):
+    critf = nn.CrossEntropyLoss()
+    lossf = op.SGD(model.parameters(), lr=0.001)
+
+    return critf, lossf
+
+
+def train_model(model, args, task, train_params):
+
+    logger.info("Training Model")
+
+    data_gen = task.load_data_generator()
+
+    criterion, optimizer = get_criterion_loss_function(model, task)
+
+    stop = False
+    while not stop:
+        # batch train
+
+        for x in data_gen:
+            pass
+
+            # optimizer.zero_grad()
+            #
+            # outputs = model(x)
+            # loss = criterion(outputs, y)
+            #
+            # loss.backward()
+            #
+            # optimizer.step()
+
+        break
+
+def save_model(model, optimizer, path):
+
+    logger.info("Saved Model")
+
 
 class Pooler(nn.Module):
     """
     Perform pooling, potentially with a projection layer beforehand.
     """
 
-    def __init__(self, project=True, pooling_type="mean"):
-        self._project = project
+    def __init__(self, project=True, inp_dim=512, out_dim=512, pooling_type="max"):
+        super(Pooler, self).__init__()
+        self._projector = nn.Linear(inp_dim, out_dim) if project else lambda x: x
         self._pooling_type = pooling_type
+        self._sequence = None
 
-    def forward(self):
+        logger.debug(f"Initializing new Pooler - project: {project}, input dims: {inp_dim}, output dims: {out_dim}, " +
+                     f"type: {pooling_type}")
+
+    def forward(self, sequence, mask):
+
+        self._sequence = self._projector(sequence)
+
         if self._pooling_type == "mean":
             pass
         elif self._pooling_type == "max":
             pass
 
-        return None
+        return self._sequence
 
 
 class Classifier(nn.Module):
@@ -27,10 +83,14 @@ class Classifier(nn.Module):
     """
 
     def __init__(self, inp_dim, n_classes):
-        self._classifier = None
+        super(Classifier, self).__init__()
+        # Start with a simple linear classifier
+        self._classifier = nn.Linear(inp_dim, n_classes)
 
-    def forward(self):
-        logits = self._classifier()
+        logger.debug(f"Initializing new Classifier - input dims: {inp_dim}, output dims: {n_classes}")
+
+    def forward(self, embedding):
+        logits = self._classifier(embedding)
 
         return logits
 
@@ -40,20 +100,45 @@ class EdgeProbingModel(nn.Module):
     Given a task and dataset, train a model of the contextual embeddings encoded information.
     """
 
-    def __init__(self):
-        self._span_extractor = None
-        self._classifier = None
+    def __init__(self, task, embedder, args):
+        super(EdgeProbingModel, self).__init__()
+        self._task = task
+
+        self._embedder = embedder
+
+        # either create a pooling object or don't
+        self._pooler = Pooler(
+            args["pooling-project"]
+        ) if args["pooling"] else None
+
+        # create a MLP classifier for the task
+        self._classifier = Classifier(512, task.get_output_dims())
+
+        logger.debug(f"Initializing new EdgeProbingModel - task: {task.get_name()}")
 
 
-    def forward(self):
+    def forward(self, batch):
         """
         Forward pass for Edge Probing models. Extract the appropriate span(s) and classify them, compute loss.
         """
         out = dict()
 
-        span_emb = None
-        logits = self._classifier(span_emb)
+        span_embedding = self._pooler() if self._pooler else batch
+        logits = self._classifier(span_embedding)
 
         out["logits"] = logits
 
         return out
+
+    def compute_loss(self):
+        """
+        Compute loss for the given batch on the specified task
+        """
+
+        return 0
+
+    def __repr__(self):
+        return f"<epnl.model.EdgeProbingModel object [N: {self._n_classes} E: {self._embedder} D: {self._data}]>"
+
+    def __str__(self):
+        return f"EdgeProbingModel <T: {str(self._task)}>"
