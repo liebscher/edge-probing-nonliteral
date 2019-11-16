8256fe1f027b31d54ac3347feb3baf871b0e2f04
Alex Liebscher <alexliebscher0@gmail.com>
2019-11-15 17:17:05 -0800

    add logging and specific set classes

--- epnl/data.py
+++ epnl/data.py
@@ -1,13 +1,20 @@
+import logging, logging.config
+
+# create logger
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('epnl')
+
 import pandas as pd
 import numpy as np
+
 
 class DataLoader:
     """
     Load a dataset
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self._path = path
 
     def num_training(self):
         return 0
@@ -22,6 +29,14 @@ class DataLoader:
 class MetaphorDataLoader(DataLoader):
     """
     Load the metaphor task dataset.
+    """
+
+    pass
+
+
+class MetonymyDataLoader(DataLoader):
+    """
+    Load the metonymy task dataset.
     """
 
     pass
